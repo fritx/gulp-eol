dcf463cc2c459f7522c0b4edb9592ca2b74b32a1
Paul Smirnov <paulsmirnov@users.noreply.github.com>
2018-01-17 10:44:33 +0300

    remove deprecated gulp-util dependency
    https://medium.com/gulpjs/gulp-util-ca3b1f9f9ac5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var through = require('through2');
-var PluginError = require('gulp-util').PluginError;
+var PluginError = require('plugin-error');
 var EOL = require('os').EOL;
 
 module.exports = function (eol, append) {
